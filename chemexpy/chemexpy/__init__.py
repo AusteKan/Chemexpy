f57d036665fe388ab856e068198b942c9e0a70f3
AusteKan <89275090+AusteKan@users.noreply.github.com>
2021-10-06 22:43:27 +0300

    Code upload v1.0.0

---
+++ chemexpy/chemexpy/__init__.py
@@ -0,0 +1,15 @@
+from . data_prep import data_prep
+from . molecule_check import molecule_check
+from . scatter_plot import scatter_plot
+from . correlation_plot import correlation_plot
+from . feature_plot import feature_plot
+from . normality_check import normality_check
+from . feature_check import feature_check
+from . feature_violinplots import feature_violinplots
+from . similarity_search import similarity_search
+from . similarity_dendogram import similarity_dendogram
+from . similarity_heatmap import similarity_heatmap
+
+
+__version__= "v1.0.0"
+
